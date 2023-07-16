ce95693d5f1cef0c3e41ed614e92c42dca1e4964
Joeh <joeh@alchemist.wtf>
2023-07-16 01:48:18 +0100

    update meta

--- src/services/meta/meta.constants.ts
+++ src/services/meta/meta.constants.ts
@@ -1,18 +1,18 @@
 export const ROUTE_META_DATA = {
   home: {
-    metaTitle: 'Balancer DeFi Liquidity Pools on [network_name]',
+    metaTitle: 'Sobal Liquidity Pools on [network_name]',
     metaDescription:
       'Explore DeFi liquidity pools or create your own. Provide liquidity to accumulate yield from swap fees while retaining your token exposure as prices move',
     metaKeywords: '',
   },
   pool: {
     metaTitle: '[pool_symbol]',
-    metaDescription: '[pool_name] — a Balancer [pool_type] pool',
+    metaDescription: '[pool_name] — a Sobal [pool_type] pool',
   },
   swap: {
     metaTitle: 'Swap DeFi tokens on [network_name]',
     metaDescription:
-      'Swap ERC-20 tokens using Balancer liquidity or with CoW protocol for gas-less signature trades, MEV protection and the best prices via intelligent routing across DeFi exchanges',
+      'Swap ERC-20 tokens using Sobal liquidity or with CoW protocol for gas-less signature trades, MEV protection and the best prices via intelligent routing across DeFi exchanges',
   },
   claim: {
     metaTitle: 'Claim liquidity mining incentives from staking',
@@ -20,9 +20,9 @@ export const ROUTE_META_DATA = {
       'LPs who stake can claim liquidity mining incentives. veBAL holders can boost incentives and claim their share of protocol revenue',
   },
   portfolio: {
-    metaTitle: 'Balancer portfolio',
+    metaTitle: 'Sobal portfolio',
     metaDescription:
-      'Track your Balancer Portfolio, including your unstaked and stake LP positions',
+      'Track your Sobal Portfolio, including your unstaked and stake LP positions',
   },
   vebal: {
     metaTitle: 'veBAL tokenomics',
@@ -30,20 +30,20 @@ export const ROUTE_META_DATA = {
       'Get veBAL (voter escrowed B-80BAL-20WETH) for extra liquidity mining incentives, governance voting power and a share of protocol revenue',
   },
   'terms-of-use': {
-    metaTitle: 'Balancer Terms of Use',
+    metaTitle: 'Sobal Terms of Use',
     metaDescription: 'The rules for the use of the website',
   },
   'privacy-policy': {
-    metaTitle: 'Balancer Privacy Policy',
+    metaTitle: 'Sobal Privacy Policy',
     metaDescription:
       'How this website collects, uses and shares personal information',
   },
   'cookies-policy': {
-    metaTitle: 'Balancer Cookie Policy',
+    metaTitle: 'Sobal Cookie Policy',
     metaDescription: 'How this website uses browser cookies',
   },
   risks: {
-    metaTitle: 'Balancer Protocol Risks',
-    metaDescription: 'The risks of using the Balancer Protocol',
+    metaTitle: 'Sobal Protocol Risks',
+    metaDescription: 'The risks of using the Sobal Protocol',
   },
 };
