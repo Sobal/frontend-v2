c5d96b0be494d48a31209ea8c46cc1ed48c719f4
Joeh <joeh@alchemist.wtf>
2023-07-18 02:44:05 +0100

    fix incorrect devnet addresses, add new pool, update sobal/sdk

--- src/lib/config/neon-devnet/pools.ts
+++ src/lib/config/neon-devnet/pools.ts
@@ -39,13 +39,14 @@ const pools: Pools = {
     // see useDisabledJoinPool.ts#nonAllowedWeightedPoolAfterTimestamp for logic.
     AllowList: [
       '0xe3f7cb8e66dc46a6479b780df45d608726d831b4000100000000000000000001',
+      '0xf327836747d49ae0ab12c75c2b474f5c63257fe4000200000000000000000002',
     ],
   },
   Factories: {
     '0x2a48D000Ff815C3AE571c5121993150B600a6AF2': 'weightedPool', // weighted pool v4
-    '0x04EF81349F5A2049D2D8261f33b7dC5bc0B33889': 'managedPool', // managed pool v2
-    '0x2297E369aa665f1cE2eCB9eBe4Ec0E6D9f02f72D': 'composableStablePool', // composable stable v5
-    '0x2818b8F2752F0AAE3938c64943aEA2a1a9258246': 'erc4626Linear', // erc4626 linear pool v4
+    '0x0a0f3a5b6Ff6e00eCBC04178DF03E769A1BD17Cc': 'managedPool', // managed pool v2
+    '0x314fF5B1e0A0F3cB7048819517aFf9B9d608b5A9': 'composableStablePool', // composable stable v5
+    '0xE1e342e62FA8c657834B285f33BDca1D0299Eedc': 'erc4626Linear', // erc4626 linear pool v4
   },
   Stakable: {
     VotingGaugePools: [],
