d4acbb08b41bc62f56251adf962aad5efc388c44
Gareth Fuller <gareth.a.f@gmail.com>
2023-08-11 10:39:12 +0100

    add missing blocktime to neon network config

--- src/lib/config/neon-mainnet/index.ts
+++ src/lib/config/neon-mainnet/index.ts
@@ -32,6 +32,7 @@ const config: Config = {
     gauge: '',
     blocks: 'https://neon-subgraph.sobal.fi/sobal-neon-blocks',
   },
+  blockTime: 2,
   bridgeUrl: 'https://neonpass.live',
   supportsEIP1559: false,
   supportsElementPools: false,
