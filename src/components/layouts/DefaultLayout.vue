03c8213c8bf1ce5edb593bffa6a513db3284288b
joehquak <joeh@sobal.fi>
2023-12-11 17:21:50 +0000

    add dynamic background to app wrapper

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -2,6 +2,36 @@
 import Footer from '@/components/footer/Footer.vue';
 import AppNav from '@/components/navs/AppNav/AppNav.vue';
 import SidebarContent from '@/components/navs/AppNav/AppSidebar/SidebarContent.vue';
+import { useRouter } from 'vue-router';
+
+const backgroundImageUrl = ref();
+
+function setBackgroundPath(routeName: string): void {
+  const backgroundImages = {
+    home: 'pool',
+    swap: 'swap',
+    pool: 'pool',
+    portfolio: 'portfolio',
+  };
+
+  backgroundImageUrl.value = backgroundImages[routeName]
+    ? `background-image: url(/images/backgrounds/${backgroundImages[routeName]}-bg.svg);
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: bottom right;`
+    : '';
+}
+
+const router = useRouter();
+
+onMounted(async () => {
+  await router.isReady();
+  setBackgroundPath(router.currentRoute.value.name as string);
+});
+
+watch(router.currentRoute, () => {
+  setBackgroundPath(router.currentRoute.value.name as string);
+});
 </script>
 
 <template>
@@ -9,7 +39,7 @@ import SidebarContent from '@/components/navs/AppNav/AppSidebar/SidebarContent.v
     <div class="hidden xl:block">
       <SidebarContent />
     </div>
-    <div class="app-body-wrapper">
+    <div class="app-body-wrapper" :style="backgroundImageUrl">
       <div class="m-2 lg:mx-5 app-body">
         <AppNav />
         <div class="pb-16 m-auto">
