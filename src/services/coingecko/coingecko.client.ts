20c555826cc688e70ae5d52144810c21861c1926
joehquak <joeh@sobal.fi>
2023-12-22 10:06:17 +0000

    feat: add caching data requests to UI for coingecko requests to prevent outages, round timestamps to EOD

--- src/services/coingecko/coingecko.client.ts
+++ src/services/coingecko/coingecko.client.ts
@@ -7,9 +7,18 @@ export class CoingeckoClient {
     this.baseUrl = 'https://api.coingecko.com/api/v3';
   }
 
+  dataCache = {};
+
   async get<T>(endpoint: string): Promise<T> {
-    const { data } = await axios.get(this.baseUrl + endpoint);
-    return data;
+    console.log(endpoint, this.dataCache);
+
+    if (!this.dataCache[endpoint]) {
+      const { data } = await axios.get(this.baseUrl + endpoint);
+      this.dataCache[endpoint] = data;
+      return data;
+    }
+
+    return this.dataCache[endpoint];
   }
 }
 
