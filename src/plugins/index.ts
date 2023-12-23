9fd3c8d8070d158213b65bc207e6285587cb25b6
joehquak <joeh@sobal.fi>
2023-12-23 14:24:04 +0000

    add solana wallets to app plugins

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -5,6 +5,21 @@ import blocknative from '@/plugins/blocknative';
 import VueVirtualScroller from 'vue3-virtual-scroller';
 import { VueQueryPlugin } from '@tanstack/vue-query';
 import VueSafeTeleport from 'vue-safe-teleport';
+import SolanaWallets from 'solana-wallets-vue';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+
+const walletOptions = {
+  wallets: [
+    new PhantomWalletAdapter(),
+    new SolflareWalletAdapter({ network: WalletAdapterNetwork.Devnet }),
+  ],
+  autoConnect: true,
+};
 
 export function registerPlugins(app: App) {
   app
@@ -13,6 +28,7 @@ export function registerPlugins(app: App) {
     .use(VueQueryPlugin)
     .use(blocknative)
     .use(VueVirtualScroller)
-    .use(VueSafeTeleport);
+    .use(VueSafeTeleport)
+    .use(SolanaWallets, walletOptions);
   return app;
 }
