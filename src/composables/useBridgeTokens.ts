93353e17b9e3d092f52981c9aae1ddd48be773f9
joehquak <joeh@sobal.fi>
2023-12-24 04:05:30 +0000

    remove unused file

--- src/composables/useBridgeTokens.ts
+++
@@ -1,9 +0,0 @@
-import { BridgeTokensProviderResponse } from '@/providers/bridge-tokens.provider';
-import { BridgeTokensProviderSymbol } from '@/providers/bridge-tokens.provider';
-import { inject } from 'vue';
-
-const defaultProviderResponse = {} as BridgeTokensProviderResponse;
-
-export const useBridgeTokens = (): BridgeTokensProviderResponse => {
-  return inject(BridgeTokensProviderSymbol, defaultProviderResponse);
-};
