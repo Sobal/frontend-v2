db9b6fff03d6fc749110848bb4c351a0c021b387
joehquak <joeh@sobal.fi>
2023-12-27 03:39:34 +0000

    add symbol and balances to state, add balance check, wallet connection checks and token selection checks. Pass props across child components and update buttons to gradient non rounded.

--- src/components/bridge/BridgeCard.vue
+++ src/components/bridge/BridgeCard.vue
@@ -5,6 +5,14 @@ import { onBeforeMount, watchEffect } from 'vue';
 import useWeb3Solana from '@/services/web3/useWeb3Solana';
 import { WalletType, WalletTypes } from '@/types/wallet';
 import { useBridgeState } from '@/composables/bridge/useBridgeState';
+import TransferPreviewModal from '@/components/bridge/modal/TransferPreviewModal.vue';
+import { useBridgeTokens } from '@/providers/bridge-tokens.provider';
+import { useTokens } from '@/providers/tokens.provider';
+
+/**
+ * STATE
+ */
+const showPreview = ref(false);
 
 const {
   tokenInAddress,
@@ -17,6 +25,10 @@ const {
   walletInConnected,
   walletInAddress,
   walletOutAddress,
+  walletInSymbol,
+  walletOutSymbol,
+  walletInBalance,
+  walletOutBalance,
   // setTokenInAddress,
   // setTokenOutAddress,
   // setInitialized,
@@ -26,6 +38,10 @@ const {
   setWalletInConnected,
   setWalletInAddress,
   setWalletOutAddress,
+  setWalletInSymbol,
+  setWalletOutSymbol,
+  setWalletInBalance,
+  setWalletOutBalance,
 } = useBridgeState();
 
 const {
@@ -34,6 +50,7 @@ const {
   account,
   disconnectWallet,
 } = useWeb3();
+
 const {
   toggleSolanaWalletSelectModal,
   isConnected,
@@ -43,6 +60,9 @@ const {
   disconnect,
   publicKeyTrimmed,
 } = useWeb3Solana();
+
+const { bridgeBalanceFor, getToken } = useBridgeTokens();
+const { balanceFor } = useTokens();
 
 const showConnectWalletModal = () => {
   if (chosenWallet.value) connect();
@@ -83,11 +103,46 @@ const setAddress = (walletType: WalletType): string => {
     : publicKeyTrimmed.value;
 };
 
+const balanceCheck = (): boolean => {
+  if (!tokenInAddress.value) return true;
+  const tokenBalance =
+    walletInType.value === WalletTypes.Solana
+      ? bridgeBalanceFor(tokenOutAddress.value, true)
+      : balanceFor(tokenInAddress.value);
+  return Number(tokenBalance) < Number(tokenInAmount.value);
+};
+
+const balance = (walletType: WalletType): string => {
+  if (!tokenInAddress.value) return '';
+  return walletType === WalletTypes.Solana
+    ? bridgeBalanceFor(tokenOutAddress.value, true)
+    : balanceFor(tokenInAddress.value);
+};
+
+const getSymbol = (): string => {
+  if (!tokenInAddress.value) return '';
+  return getToken(tokenInAddress.value).symbol;
+};
+
+const swapDisabled = computed(() => {
+  const walletNotConnected = !isWalletReady && !isConnected;
+  const hasAmountsError =
+    !tokenInAmount.value || Number(tokenInAmount.value) <= 0;
+  const tokenNotSelected = !tokenInAddress.value;
+  return (
+    walletNotConnected || hasAmountsError || tokenNotSelected || balanceCheck()
+  );
+});
+
 watchEffect(() => {
   setWalletInAddress(setAddress(walletInType.value));
   setWalletInConnected(setConnected(walletInType.value));
   setWalletOutAddress(setAddress(walletOutType.value));
   setWalletOutConnected(setConnected(walletOutType.value));
+  setWalletInSymbol(getSymbol());
+  setWalletOutSymbol(getSymbol());
+  setWalletInBalance(balance(walletInType.value));
+  setWalletOutBalance(balance(walletOutType.value));
 });
 
 onBeforeMount(() => {
@@ -103,10 +158,14 @@ onBeforeMount(() => {
     <div class="flex flex-col">
       <BridgePair
         v-model:tokenInAmount="tokenInAmount"
-        v-model:tokenInAddress="tokenInAddress"
+        v-model:evmTokenAddress="tokenInAddress"
         v-model:tokenOutAmount="tokenOutAmount"
-        v-model:tokenOutAddress="tokenOutAddress"
+        v-model:solanaTokenAddress="tokenOutAddress"
         v-model:walletInType="walletInType"
+        v-model:walletInBalance="walletInBalance"
+        v-model:walletOutBalance="walletOutBalance"
+        v-model:walletInSymbol="walletInSymbol"
+        v-model:walletOutSymbol="walletOutSymbol"
         v-model:walletOutType="walletOutType"
         v-model:walletInConnected="walletInConnected"
         v-model:walletOutConnected="walletOutConnected"
@@ -119,26 +178,38 @@ onBeforeMount(() => {
         v-if="!isWalletReady && !isConnected"
         class="mt-5"
         label="Connect Wallets"
-        rounded
-        color="green"
+        block
+        color="gradient"
         @click="startConnectWithInjectedProvider"
       />
       <BalBtn
         v-else-if="!isWalletReady"
         class="mt-5"
-        label="Connect Neon Wallet"
-        rounded
-        color="green"
+        label="Connect Wallet"
+        block
+        color="gradient"
         @click="startConnectWithInjectedProvider"
       />
       <BalBtn
         v-else-if="!isConnected"
         class="mt-5"
         :label="content"
-        rounded
-        color="green"
+        block
+        color="gradient"
         @click="showConnectWalletModal"
       />
+      <BalBtn
+        v-else
+        :label="$t('preview')"
+        :disabled="swapDisabled"
+        class="mt-5"
+        color="gradient"
+        block
+        @click="showPreview = true"
+      />
     </div>
+    <teleport to="#modal">
+      <TransferPreviewModal v-if="showPreview" @close="showPreview = false" />
+    </teleport>
   </div>
 </template>
