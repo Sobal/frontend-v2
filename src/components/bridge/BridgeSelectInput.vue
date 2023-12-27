448854202fd2972c09c24c6ea2b01be162f3529c
joehquak <joeh@sobal.fi>
2023-12-27 03:43:22 +0000

    remove unused code, add copy to lang

--- src/components/bridge/BridgeSelectInput.vue
+++ src/components/bridge/BridgeSelectInput.vue
@@ -2,7 +2,6 @@
 import { computed, ref, watchEffect } from 'vue';
 import { TokenInfo } from '@/types/TokenList';
 import useNumbers from '@/composables/useNumbers';
-import { isSameAddress } from '@/lib/utils';
 import SelectBridgeTokenModal from '@/components/bridge/SelectBridgeTokenModal.vue';
 import { useTokens } from '@/providers/tokens.provider';
 
@@ -32,20 +31,17 @@ const emit = defineEmits<{
  * STATE
  */
 const openTokenModal = ref(false);
-const optionTokens = ref<Record<string, TokenInfo>>({});
 
 /**
  * COMPOSABLEs
  */
 const { fNum } = useNumbers();
-const { getToken, tokens } = useTokens();
+const { getToken } = useTokens();
 
 /**
  * COMPUTED
  */
 const hasToken = computed(() => !!props.modelValue);
-
-const computedOptions = computed(() => tokens.value);
 
 const token = computed((): TokenInfo | null => {
   if (!hasToken.value) return null;
@@ -95,56 +91,6 @@ watchEffect(() => {
         class="ml-2 text-green-600 group-hover:text-purple-500 dark:text-green-600 dark:group-hover:text-pink-400 transition-colors"
       />
     </div>
-    <BalDropdown
-      v-else-if="hasToken && fixed && options.length > 0"
-      :options="options"
-      minWidth="40"
-      @selected="emit('update:modelValue', $event)"
-    >
-      <template #activator>
-        <div class="group token-select-input selected selectable">
-          <div class="w-8">
-            <BalAsset :address="token?.address" class="shadow" />
-          </div>
-          <span class="text-base font-medium">
-            {{ token?.symbol }}
-          </span>
-          <!--          <span v-if="Number(weight) > 0" class="ml-2 text-secondary">-->
-          <!--            {{-->
-          <!--              fNum(weight, {-->
-          <!--                style: 'percent',-->
-          <!--                maximumFractionDigits: 0,-->
-          <!--              })-->
-          <!--            }}-->
-          <!--          </span>-->
-          test
-          <BalIcon
-            name="chevron-down"
-            size="sm"
-            class="ml-2 text-blue-500 group-hover:text-purple-500 dark:text-blue-400 dark:group-hover:text-purple-400 transition-colors"
-          />
-        </div>
-      </template>
-      <template #option="{ option: address }">
-        <div :set="computedOptions" class="flex justify-between items-center">
-          <div class="flex items-center">
-            <BalAsset
-              :address="optionTokens[address]?.address"
-              class="shadow"
-            />
-            <span class="ml-1 font-medium">
-              {{ optionTokens[address]?.symbol }}
-            </span>
-          </div>
-          <BalIcon
-            v-if="isSameAddress(optionTokens[address].address, modelValue)"
-            name="check"
-            class="ml-4 text-blue-500 dark:text-blue-400"
-          />
-        </div>
-      </template>
-    </BalDropdown>
-
     <div
       v-else
       :class="['token-select-input unselected', { selectable: !fixed }]"
@@ -153,10 +99,9 @@ watchEffect(() => {
       <div v-if="!fixed">
         {{ $t('selectToken') }}
       </div>
-      <div v-else>Select token above</div>
+      <div v-else>{{ $t('selectTokenAbove') }}</div>
       <BalIcon v-if="!fixed" name="chevron-down" size="sm" class="ml-2" />
     </div>
-
     <teleport to="#modal">
       <SelectBridgeTokenModal
         v-if="openTokenModal"
