1fd9b517e66334be64dc1c287fbecdd15126d94d
joehquak <joeh@sobal.fi>
2023-12-30 06:32:05 +0000

    use variable for bridge rpc, change connection type

--- src/services/token/bridge-token.service.ts
+++ src/services/token/bridge-token.service.ts
@@ -1,23 +1,22 @@
 import { Connection } from '@solana/web3.js';
 
-import { configService as _configService } from '../config/config.service';
+import { configService as _configService } from '@/services/config/config.service';
 import BalancesConcern from './bridge-concerns/balances.concern';
 
+const bridgeRpc = _configService.network.bridgeRpc;
+
 export default class BridgeTokenService {
-  provider: Connection;
+  connection: Connection;
   balances: BalancesConcern;
 
   constructor(
     readonly balancesConcernClass = BalancesConcern,
-    readonly rpcProviderService = new Connection(
-      'https://mainnet.helius-rpc.com/?api-key=804a681b-735c-4970-bbcc-b64e84575d24',
-      {
-        commitment: 'confirmed',
-      }
-    ),
+    readonly rpcProviderService = new Connection(bridgeRpc, {
+      commitment: 'confirmed',
+    }),
     readonly configService = _configService
   ) {
-    this.provider = this.rpcProviderService;
+    this.connection = this.rpcProviderService;
     this.balances = new balancesConcernClass(this);
   }
 }
