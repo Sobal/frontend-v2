b480eaa4ae88ebf0dc69a4ae69ba495bb447aee3
joehquak <joeh@sobal.fi>
2024-01-02 01:36:32 +0000

    show native tokens on tokenlist

--- src/components/bridge/SelectBridgeTokenModal.vue
+++ src/components/bridge/SelectBridgeTokenModal.vue
@@ -25,7 +25,7 @@ const props = withDefaults(defineProps<Props>(), {
   open: false,
   excludedTokens: () => [],
   subset: () => [],
-  includeEther: false,
+  includeEther: true,
   hideTokenLists: true,
   ignoreBalances: false,
 });
