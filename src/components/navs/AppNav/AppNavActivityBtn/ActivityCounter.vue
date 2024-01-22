f22a16511ca8092c0ab69840b291340658675618
joehquak <joeh@sobal.fi>
2024-01-22 20:08:53 +0000

    update animation colour

--- src/components/navs/AppNav/AppNavActivityBtn/ActivityCounter.vue
+++ src/components/navs/AppNav/AppNavActivityBtn/ActivityCounter.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="relative text-orange-500">
+  <span class="relative text-blue-500">
     <span
       class="absolute top-1/2 left-1/2 text-sm -translate-x-1/2 -translate-y-1/2"
     >
