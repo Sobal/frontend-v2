24a1afa6a8a568f1dad09a1da61a17fe60edd597
joehquak <joeh@sobal.fi>
2024-01-22 18:32:49 +0000

    simplify account button on header

--- src/components/navs/AppNav/AppNavAccountBtn.vue
+++ src/components/navs/AppNav/AppNavAccountBtn.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-
 import Avatar from '@/components/images/Avatar.vue';
 import useBreakpoints from '@/composables/useBreakpoints';
 import useWeb3 from '@/services/web3/useWeb3';
@@ -8,18 +6,8 @@ import { shorten } from '@/lib/utils';
 
 import AppNavSettings from './AppNavSettings.vue';
 
-const { bp, upToLargeBreakpoint, isMobile } = useBreakpoints();
+const { isMobile } = useBreakpoints();
 const { isLoadingProfile, profile, account } = useWeb3();
-
-const avatarSize = computed(() => {
-  if (bp.value === 'sm') {
-    return 35;
-  } else if (['md', 'lg'].includes(bp.value)) {
-    return 40;
-  } else {
-    return 20;
-  }
-});
 </script>
 
 <template>
@@ -29,20 +17,22 @@ const avatarSize = computed(() => {
     :detached="isMobile ? true : undefined"
   >
     <template #activator>
-      <BalBtn
-        class="text-base"
-        :class="{ btn: upToLargeBreakpoint }"
-        :loading="isLoadingProfile"
-        :loadingLabel="upToLargeBreakpoint ? '' : $t('connecting')"
-        color="white"
-        :size="upToLargeBreakpoint ? 'md' : 'sm'"
-        :circle="upToLargeBreakpoint"
-      >
-        <Avatar
-          :iconURI="profile?.avatar || ''"
-          :address="account"
-          :size="avatarSize"
-        />
+      <div class="flex flex-row place-items-center cursor-pointer">
+        <BalBtn
+          class="text-base"
+          :loading="isLoadingProfile"
+          loadingLabel=""
+          color="white"
+          size="md"
+          circle
+        >
+          <Avatar
+            :iconURI="profile?.avatar || ''"
+            :address="account"
+            :size="40"
+          />
+        </BalBtn>
+
         <span
           v-if="profile && profile.ens"
           class="hidden lg:inline-block pl-2"
@@ -53,7 +43,7 @@ const avatarSize = computed(() => {
           class="hidden lg:inline-block pl-2 eth-address"
           v-text="shorten(account, 4, 4)"
         />
-      </BalBtn>
+      </div>
     </template>
     <AppNavSettings />
   </BalPopover>
