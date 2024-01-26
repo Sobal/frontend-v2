883038d6c2ec4a5db1f2c78884ce3c7f39535fd1
joehquak <joeh@sobal.fi>
2024-01-26 04:08:36 +0000

    add walletconnect support

--- src/plugins/solanawallets.ts
+++ src/plugins/solanawallets.ts
@@ -5,7 +5,7 @@ import {
   CoinbaseWalletAdapter,
   TrustWalletAdapter,
   TorusWalletAdapter,
-  // WalletConnectWalletAdapter,
+  WalletConnectWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
 
 import { configService } from '@/services/config/config.service';
@@ -26,23 +26,22 @@ export const SolanaWalletOptions =
           new SolflareWalletAdapter({
             network: WalletAdapterNetwork[solanaRpcName],
           }),
-          // TODO: Only compatible with WSS - Find WSS compatible rpcs
-          // new WalletConnectWalletAdapter({
-          //   network: WalletAdapterNetwork[solanaRpcName],
-          //   options: {
-          //     relayUrl: solanaRpcUrl,
-          //     projectId: '96ec07ab805a6abb425fb22135bd41a1',
-          //     metadata: {
-          //       name: 'Sobal',
-          //       description:
-          //         'Bringing you the best AMM, trading and DeFi experience by leveraging the power of flexible and powerful Smart Contracts',
-          //       url: 'https://app.sobal.fi',
-          //       icons: [
-          //         'https://avatars.githubusercontent.com/u/106710686?s=200',
-          //       ],
-          //     },
-          //   },
-          // }),
+          new WalletConnectWalletAdapter({
+            network: WalletAdapterNetwork[solanaRpcName],
+            options: {
+              relayUrl: solanaRpcUrl,
+              projectId: '96ec07ab805a6abb425fb22135bd41a1',
+              metadata: {
+                name: 'Sobal',
+                description:
+                  'Bringing you the best AMM, trading and DeFi experience by leveraging the power of flexible and powerful Smart Contracts',
+                url: 'https://app.sobal.fi',
+                icons: [
+                  'https://avatars.githubusercontent.com/u/106710686?s=200',
+                ],
+              },
+            },
+          }),
         ],
         autoConnect: false,
       }
