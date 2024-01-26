dee63beed69969071c68bc0c3f3dbc31b9ffdd41
joehquak <joeh@sobal.fi>
2024-01-26 03:39:25 +0000

    introduce sorting wallets by name and state

--- src/components/web3/SolanaSelectModal.vue
+++ src/components/web3/SolanaSelectModal.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import useWeb3Solana from '@/services/web3/useWeb3Solana';
 import SolanaWalletButton from '@/components/web3/SolanaWalletButton.vue';
+import { WalletReadyState } from '@solana/wallet-adapter-base';
 
 interface Props {
   isVisible?: boolean;
@@ -13,16 +14,27 @@ const props = withDefaults(defineProps<Props>(), {
 const emit = defineEmits(['close']);
 
 const { allowedWallets } = useWeb3Solana();
+
+const stateOrder = {
+  [WalletReadyState.Installed]: 0,
+  [WalletReadyState.Loadable]: 1,
+  [WalletReadyState.NotDetected]: 2,
+  [WalletReadyState.Unsupported]: 3,
+};
+
+const sortedWallets = allowedWallets.value
+  .sort((a, b) => a.adapter.name.localeCompare(b.adapter.name))
+  .sort((a, b) => stateOrder[a.readyState] - stateOrder[b.readyState]);
 </script>
 
 <template>
   <BalModal
-    title="Connect to a Solana wallet"
+    :title="$t('connectWalletSolana')"
     :show="props.isVisible"
     @close="emit('close')"
   >
     <SolanaWalletButton
-      v-for="wallet in allowedWallets"
+      v-for="wallet in sortedWallets"
       :key="wallet.adapter.name"
       :wallet="wallet.adapter"
       :walletState="wallet.readyState"
