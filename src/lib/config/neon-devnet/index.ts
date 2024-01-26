fdc90c630c1f197389c5cbdae3d1436643e49814
joehquak <joeh@sobal.fi>
2024-01-26 04:08:06 +0000

    add websocket services for Neon Mainnet and Devnet

--- src/lib/config/neon-devnet/index.ts
+++ src/lib/config/neon-devnet/index.ts
@@ -29,7 +29,7 @@ const config: Config = {
   balancerApi: 'https://api.sobal.fi',
   poolsUrlV2: '',
   solanaRpc:
-    'https://devnet.helius-rpc.com/?api-key=804a681b-735c-4970-bbcc-b64e84575d24',
+    'wss://devnet.helius-rpc.com/?api-key=804a681b-735c-4970-bbcc-b64e84575d24',
   solanaRpcName: 'devnet',
   bridgeUi: true,
   subgraphs: {
--- src/lib/config/neon-mainnet/index.ts
+++ src/lib/config/neon-mainnet/index.ts
@@ -36,7 +36,7 @@ const config: Config = {
   blockTime: 4,
   bridgeUrl: 'https://neonpass.live',
   solanaRpc:
-    'https://mainnet.helius-rpc.com/?api-key=804a681b-735c-4970-bbcc-b64e84575d24',
+    'wss://mainnet.helius-rpc.com/?api-key=804a681b-735c-4970-bbcc-b64e84575d24',
   solanaRpcName: 'mainnet-beta',
   solanaExplorer: 'https://solscan.io',
   solanaExplorerSuffix: '',
